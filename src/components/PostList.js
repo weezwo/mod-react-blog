3961df84ccd4aaf7f38c319ae2ed922ded15067a
Luisa Scavo <luisascavo@gmail.com>
2019-03-13 14:39:58 -0400

    stub out connection to action

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+    
     render(){
         return <div className="ui container">Post List</div>
     }
 }
 
-export default PostList;
+export default connect(null, { fetchPosts })(PostList); 
+// first argument to connect is always mapStateToProps -- we don't have that yet.
+// Second arguement is equivalent to { fetchPosts: fetchPosts }
