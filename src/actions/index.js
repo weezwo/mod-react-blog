63eba75d8ec231a055345b03b1b471f9fb609223
Luisa Scavo <luisascavo@gmail.com>
2019-03-14 20:23:59 -0400

    write fetchPosts action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
-export const fetchPosts = () => {
-  return {
-    type: 'FETCH_POSTS'
-  };
-};
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async (dispatch) => {
+    const resp = await jsonPlaceholder.get('/posts')
+    dispatch({ type: 'FETCH_POSTS', payload: resp})};
