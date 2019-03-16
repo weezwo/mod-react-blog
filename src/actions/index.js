5405004c9d9bf513e75c5a4b77d019f63e097510
Luisa Scavo <luisascavo@gmail.com>
2019-03-16 15:30:00 -0400

    add fetchUser action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,4 +2,10 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async (dispatch) => {
     const resp = await jsonPlaceholder.get('/posts')
-    dispatch({ type: 'FETCH_POSTS', payload: resp.data})};
+    dispatch({ type: 'FETCH_POSTS', payload: resp.data})
+  };
+
+export const fetchUser = (id) => async (dispatch) => {
+  const resp = await jsonPlaceholder.get(`/users/${id}`)
+  dispatch({ type: 'FETCH_USER', payload: resp.data})
+};
