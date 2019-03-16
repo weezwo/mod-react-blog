c91345268b339217c03ebd19e09f8eae965060f2
Luisa Scavo <luisascavo@gmail.com>
2019-03-15 20:46:55 -0400

    write mapStateToProps

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,12 +6,17 @@ class PostList extends React.Component {
     componentDidMount() {
         this.props.fetchPosts();
     }
-    
+
     render(){
+        console.log(this.props.posts)
         return <div className="ui container">Post List</div>
     }
 }
 
-export default connect(null, { fetchPosts })(PostList); 
-// first argument to connect is always mapStateToProps -- we don't have that yet.
+const mapStateToProps = (state) => {
+    return { posts: state.posts }
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostList); 
+// first argument to connect is always mapStateToProps
 // Second arguement is equivalent to { fetchPosts: fetchPosts }
