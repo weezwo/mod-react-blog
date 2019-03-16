44bcf5dfaea7493d378a56275f1e98ab21da48cd
Luisa Scavo <luisascavo@gmail.com>
2019-03-16 15:37:07 -0400

    refactor, use ownProps in mapStateToProps

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -7,7 +7,7 @@ class UserHeader extends React.Component {
     this.props.fetchUser(this.props.userId)
   }
   render() {
-    const user = this.props.users
+    const { user } = this.props;
 
     if (!user) {
       return null;
@@ -17,8 +17,8 @@ class UserHeader extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return { users: state.users.find(user => user.id === this.props.userId) }
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) }
 }
 
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
