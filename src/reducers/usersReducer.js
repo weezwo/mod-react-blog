47b92002e8dcdcf92dd1803ff787c592f5cbb69a
Luisa Scavo <luisascavo@gmail.com>
2019-03-16 15:30:16 -0400

    initial usersReducer

---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,11 @@
+import { stat } from "fs";
+
+export default (state = [], action) => {
+  switch (action.type) {
+    case 'FETCH_USER':
+      return [...state, action.payload];
+    default:
+      return state;
+
+  }
+};
