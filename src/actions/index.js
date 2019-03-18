b3079dfb131e3dee025001a145384f117b64c635
Luisa Scavo <luisascavo@gmail.com>
2019-03-18 17:10:50 -0400

    refactor using _.chain

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,8 +3,16 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
-  const userIds = _.uniq(_.map(getState().posts, 'userId'))
-  userIds.forEach(id => dispatch(fetchUser(id)))
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'))
+  // userIds.forEach(id => dispatch(fetchUser(id)))
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value() // okay, go ahead -- execute the chain
+
+    // this works exactly as the above comment does -- just an alternative method using lodash's .chain function.
 };
 
 export const fetchPosts = () => async (dispatch) => {
