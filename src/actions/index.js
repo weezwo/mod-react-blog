e6026f7a24680ee7b3aa92cdc9c8f616c2dbb796
Luisa Scavo <luisascavo@gmail.com>
2019-03-18 17:12:25 -0400

    add explanation of fetchPostsAndUsers

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,6 +12,7 @@ export const fetchPostsAndUsers = () => async (dispatch, getState) => {
     .forEach(id => dispatch(fetchUser(id)))
     .value() // okay, go ahead -- execute the chain
 
+    // here, we prevent overfetching by only fetching each user once
     // this works exactly as the above comment does -- just an alternative method using lodash's .chain function.
 };
 
