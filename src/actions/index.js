1ffd5e5d73e859b110079c9a8a9d416615757ae0
Luisa Scavo <luisascavo@gmail.com>
2019-03-18 17:04:25 -0400

    add fetchPostsAndUsers action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,15 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, 'userId'))
+  userIds.forEach(id => dispatch(fetchUser(id)))
+};
+
 export const fetchPosts = () => async (dispatch) => {
-    const resp = await jsonPlaceholder.get('/posts')
-    dispatch({ type: 'FETCH_POSTS', payload: resp.data})
+  const resp = await jsonPlaceholder.get('/posts')
+  dispatch({ type: 'FETCH_POSTS', payload: resp.data})
   };
 
 export const fetchUser = (id) => async (dispatch) => {
